00cd149b32f826544a683811c9c1259e1ea79718
Andrea Celentano <andrea.celentano@ge.infn.it>
2016-11-16 17:19:44 +0100

    moving folders

--- macros/loadRootLibs.C
+++
@@ -1,6 +0,0 @@
-loadRootLibs(){
-	 gSystem->Load("${OPTO}/lib/libCommonClassesDict.so");
-	 gSystem->Load("${OPTO}/lib/libTofpetClassesDict.so");
-	 gSystem->Load("${OPTO}/lib/libOpNoviceClassesDict.so");
-  	 gSystem->Load("${OPTO}/lib/libReconstructionNewClassesDict.so");
-}
