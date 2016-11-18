ae13709a40a1c66c89d8668bd56de12e4d38a0f0
Andrea Celentano <andrea.celentano@ge.infn.it>
2016-11-18 14:47:14 +0100

    removed build folders

--- software/packages/Simulation/build/CMakeFiles/OpNovice.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/OpNovice"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/OpNovice.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
